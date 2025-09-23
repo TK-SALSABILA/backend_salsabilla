55cef823ac4f34e4e17fd6a4325e21e285c8dfb5
ojan <milyasa2468@gmail.com>
2025-09-23 09:35:28 +0700

    fix:(SecurityConfig) fixing cors patch

--- src/main/java/org/school/backend/adapters/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/org/school/backend/adapters/infrastructure/configuration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
             )) {
                 res.setHeader("Access-Control-Allow-Origin", origin);
             }
-            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
             res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
             res.setHeader("Access-Control-Allow-Credentials", "true");
 
